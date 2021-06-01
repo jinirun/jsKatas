197f5a6d2366f3858876249360611cd17301172a
Ranjini <ranj.jini@gmail.com>
2021-06-01 15:02:23 +0100

    Added test cases to return the correct output

--- Solutions/getScreenTimeAlertList.js
+++ Solutions/getScreenTimeAlertList.js
@@ -1,4 +1,7 @@
 function getScreenTimeAlertList(userArray, date){
+  let userList = [];
+  let userScreenTime = 0;
+  let screenTime = [];
 
   if (userArray === undefined){
     throw new Error("Error: Input cannot be empty");
@@ -7,7 +10,22 @@ function getScreenTimeAlertList(userArray, date){
   if(date === undefined){
     throw new Error("Error: Input cannot be empty");
   }
-  return [];
+
+  for(let i=0; i<userArray.length; i++){
+    for(let j = 0; j<userArray[i].screenTime.length; j++){
+      screenTime = Object.values(userArray[i].screenTime[j].usage);
+      if(userArray[i].screenTime[j].date === date){
+
+        for(let k=0; k<screenTime.length; k++){
+          userScreenTime += screenTime[k];
+        }
+        if (userScreenTime > 100){
+          userList.push(userArray[i].username);
+        }
+      }
+    }
+  }
+  return userList;
 }
 
 module.exports = {
--- Tests/getScreenTimeAlertList.test.js
+++ Tests/getScreenTimeAlertList.test.js
@@ -2,10 +2,53 @@ const {
   getScreenTimeAlertList
 }= require("../Solutions/getScreenTimeAlertList");
 
-describe("getScreenTimealertList", () => {
+describe("getScreenTimeAlertList", () => {
   test("Returns an error if the object list is empty", () => {
     expect(() => {
       getScreenTimeAlertList();
     }).toThrow("Error: Input cannot be empty");
+  })
+
+  test("Returns an array of users if input is valid", () => {
+    expect(getScreenTimeAlertList([
+        {
+          username: "beth_1234",
+          name: "Beth Smith",
+          screenTime: [{ date: "2019-05-01", usage: { twitter: 34, instagram: 22, facebook: 40} },
+            { date: "2019-05-02", usage: { twitter: 56, instagram: 40, facebook: 31} },
+            { date: "2019-05-03", usage: { twitter: 12, instagram: 15, facebook: 19} },
+            { date: "2019-05-04", usage: { twitter: 10, instagram: 56, facebook: 61} },
+          ]
+        },
+        {
+          username: "sam_j_1989",
+          name: "Sam Jones",
+          screenTime: [
+            { date: "2019-06-11", usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 10} },
+            { date: "2019-06-13", usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 16} },
+            { date: "2019-06-14", usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 31} },
+          ]
+        }
+      ], "2019-05-02")).toEqual(["beth_1234"]);
+    expect(getScreenTimeAlertList([
+      {
+        username: "beth_1234",
+        name: "Beth Smith",
+        screenTime: [{ date: "2019-05-01", usage: { twitter: 34, instagram: 22, facebook: 40} },
+          { date: "2019-05-02", usage: { twitter: 56, instagram: 40, facebook: 4} },
+          { date: "2019-05-03", usage: { twitter: 12, instagram: 15, facebook: 19} },
+          { date: "2019-05-04", usage: { twitter: 10, instagram: 56, facebook: 61} },
+        ]
+      },
+      {
+        username: "sam_j_1989",
+        name: "Sam Jones",
+        screenTime: [
+          { date: "2019-06-11", usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 10} },
+          { date: "2019-06-13", usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 16} },
+          { date: "2019-06-14", usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 31} },
+        ]
+      }
+    ], "2019-05-02")).toEqual([]);
   })
 })
